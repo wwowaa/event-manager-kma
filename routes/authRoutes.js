1a07db856a898e715a9f9537179844807bc730ee
Andrii Chernikov <2834980C@student.gla.ac.uk>
2023-06-23 14:00:27 +0100

    Current user API

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,5 +10,6 @@ router.post('/signup', authContoller.signup_post);
 router.get('/auth', authContoller.auth_get);
 router.post('/auth', authContoller.auth_post);
 router.get('/signout', authContoller.signout_get);
+router.get('/currentuser', authContoller.current_user_get );
 
 module.exports = router; 
