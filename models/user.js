fdf81b70afbd005bd0430ddb6d39709292062754
Andrii Chernikov <2834980C@student.gla.ac.uk>
2023-06-23 19:05:08 +0100

    hash encrypting confirmPassword

--- models/user.js
+++ models/user.js
@@ -41,6 +41,7 @@ userSchema.pre('save', async function (next) {
 
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
+    this.confirmPassword = this.password;
     next();
 });
 
